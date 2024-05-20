44782db7fd6d6aa56a11f67d045a88a2fdc6c7d4
James Yuri <yuridevs.ke@gmail.com>
2024-05-21 01:58:27 +0300

    app

---
+++ codewave/render/MERN Task Manager App/server/controllers/userController.js
@@ -0,0 +1,266 @@
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import createJWT from "../utils/index.js";
+import Notice from "../models/notis.js";
+
+// POST request - login user
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res
+      .status(401)
+      .json({ status: false, message: "Invalid email or password." });
+  }
+
+  if (!user?.isActive) {
+    return res.status(401).json({
+      status: false,
+      message: "User account has been deactivated, contact the administrator",
+    });
+  }
+
+  const isMatch = await user.matchPassword(password);
+
+  if (user && isMatch) {
+    createJWT(res, user._id);
+
+    user.password = undefined;
+
+    res.status(200).json(user);
+  } else {
+    return res
+      .status(401)
+      .json({ status: false, message: "Invalid email or password" });
+  }
+});
+
+// POST - Register a new user
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, isAdmin, role, title } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    return res
+      .status(400)
+      .json({ status: false, message: "Email address already exists" });
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    isAdmin,
+    role,
+    title,
+  });
+
+  if (user) {
+    isAdmin ? createJWT(res, user._id) : null;
+
+    user.password = undefined;
+
+    res.status(201).json(user);
+  } else {
+    return res
+      .status(400)
+      .json({ status: false, message: "Invalid user data" });
+  }
+});
+
+// POST -  Logout user / clear cookie
+const logoutUser = (req, res) => {
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logged out successfully" });
+};
+
+// @GET -   Get user profile
+// const getUserProfile = asyncHandler(async (req, res) => {
+//   const { userId } = req.user;
+
+//   const user = await User.findById(userId);
+
+//   user.password = undefined;
+
+//   if (user) {
+//     res.json({ ...user });
+//   } else {
+//     res.status(404);
+//     throw new Error("User not found");
+//   }
+// });
+
+const getTeamList = asyncHandler(async (req, res) => {
+  const { search } = req.query;
+  let query = {};
+
+  if (search) {
+    const searchQuery = {
+      $or: [
+        { title: { $regex: search, $options: "i" } },
+        { name: { $regex: search, $options: "i" } },
+        { role: { $regex: search, $options: "i" } },
+        { email: { $regex: search, $options: "i" } },
+      ],
+    };
+    query = { ...query, ...searchQuery };
+  }
+
+  const user = await User.find(query).select("name title role email isActive");
+
+  res.status(201).json(user);
+});
+
+// @GET  - get user notifications
+const getNotificationsList = asyncHandler(async (req, res) => {
+  const { userId } = req.user;
+
+  const notice = await Notice.find({
+    team: userId,
+    isRead: { $nin: [userId] },
+  })
+    .populate("task", "title")
+    .sort({ _id: -1 });
+
+  res.status(201).json(notice);
+});
+
+// @GET  - get user notifications
+const markNotificationRead = asyncHandler(async (req, res) => {
+  try {
+    const { userId } = req.user;
+    const { isReadType, id } = req.query;
+
+    if (isReadType === "all") {
+      await Notice.updateMany(
+        { team: userId, isRead: { $nin: [userId] } },
+        { $push: { isRead: userId } },
+        { new: true }
+      );
+    } else {
+      await Notice.findOneAndUpdate(
+        { _id: id, isRead: { $nin: [userId] } },
+        { $push: { isRead: userId } },
+        { new: true }
+      );
+    }
+    res.status(201).json({ status: true, message: "Done" });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// PUT - Update user profile
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const { userId, isAdmin } = req.user;
+  const { _id } = req.body;
+
+  const id =
+    isAdmin && userId === _id
+      ? userId
+      : isAdmin && userId !== _id
+      ? _id
+      : userId;
+
+  const user = await User.findById(id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    // user.email = req.body.email || user.email;
+    user.title = req.body.title || user.title;
+    user.role = req.body.role || user.role;
+
+    const updatedUser = await user.save();
+
+    user.password = undefined;
+
+    res.status(201).json({
+      status: true,
+      message: "Profile Updated Successfully.",
+      user: updatedUser,
+    });
+  } else {
+    res.status(404).json({ status: false, message: "User not found" });
+  }
+});
+
+// PUT - active/disactivate user profile
+const activateUserProfile = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  const user = await User.findById(id);
+
+  if (user) {
+    user.isActive = req.body.isActive;
+
+    await user.save();
+
+    user.password = undefined;
+
+    res.status(201).json({
+      status: true,
+      message: `User account has been ${
+        user?.isActive ? "activated" : "disabled"
+      }`,
+    });
+  } else {
+    res.status(404).json({ status: false, message: "User not found" });
+  }
+});
+
+const changeUserPassword = asyncHandler(async (req, res) => {
+  const { userId } = req.user;
+
+  // Remove this condition
+  if (userId === "65ff94c7bb2de638d0c73f63") {
+    return res.status(404).json({
+      status: false,
+      message: "This is a test user. You can not chnage password. Thank you!!!",
+    });
+  }
+
+  const user = await User.findById(userId);
+
+  if (user) {
+    user.password = req.body.password;
+
+    await user.save();
+
+    user.password = undefined;
+
+    res.status(201).json({
+      status: true,
+      message: `Password chnaged successfully.`,
+    });
+  } else {
+    res.status(404).json({ status: false, message: "User not found" });
+  }
+});
+
+// DELETE - delete user account
+const deleteUserProfile = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  await User.findByIdAndDelete(id);
+
+  res.status(200).json({ status: true, message: "User deleted successfully" });
+});
+
+export {
+  activateUserProfile,
+  changeUserPassword,
+  deleteUserProfile,
+  getTeamList,
+  loginUser,
+  logoutUser,
+  registerUser,
+  updateUserProfile,
+  getNotificationsList,
+  markNotificationRead,
+};
---
+++ codewave/render/MERN Task Manager App/server/middleware/authMiddleware.js
@@ -0,0 +1,47 @@
+import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+
+const protectRoute = asyncHandler(async (req, res, next) => {
+  let token = req.cookies.token;
+
+  if (token) {
+    try {
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+      const resp = await User.findById(decodedToken.userId).select(
+        "isAdmin email"
+      );
+
+      req.user = {
+        email: resp.email,
+        isAdmin: resp.isAdmin,
+        userId: decodedToken.userId,
+      };
+
+      next();
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(401)
+        .json({ status: false, message: "Not authorized. Try login again." });
+    }
+  } else {
+    return res
+      .status(401)
+      .json({ status: false, message: "Not authorized. Try login again." });
+  }
+});
+
+const isAdminRoute = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    return res.status(401).json({
+      status: false,
+      message: "Not authorized as admin. Try login as admin.",
+    });
+  }
+};
+
+export { isAdminRoute, protectRoute };
